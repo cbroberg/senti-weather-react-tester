a021f3a6b5f131952be65e3df11954f625d78fea
Christian Broberg <cb@webhouse.dk>
2018-09-20 13:09:25 +0200

    Push code

---
+++ src/GetData.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { create } from 'apisauce'
+
+const api = create({
+    baseURL: 'http://api.openweathermap.org/data/2.5/'
+})
+
+export const GetWeather = async (parameter) => {
+    var data;
+    if (typeof (parameter) === 'object') {
+        var obj = await api.get('weather?lat=' + parameter.lat + '&lon=' + parameter.lon + '&appid=cbabea7dfe88ded04409be196b518dab')
+            .then((res) => {
+                if (res.ok) {
+                    data = res.data;
+                }
+            })
+    } else {
+        var obj = await api.get('weather?q=' + parameter + '&appid=cbabea7dfe88ded04409be196b518dab')
+            .then((res) => {
+                if (res.ok){
+                    data = res.data;
+                }
+            })
+    }
+    if (data) return data;
+}
---
+++ src/Weather.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { GetWeather } from './GetData'
+
+class Weather extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            weatherObj: null,
+        }
+    }
+
+    componentWillMount = async () => {
+        const coords = {
+            lat: 57.043271,
+            lon: 9.921155,
+        }
+        const cityName = 'Aalborg'
+        this.setState({ weatherObj: await GetWeather(cityName) })
+    }
+
+    render () {
+        const { weatherObj } = this.state;
+        console.log(weatherObj)
+        return (
+            <div>
+                {weatherObj ? `Weather in ${weatherObj.name} , ${weatherObj.sys.country} : ${weatherObj.weather[0].main} - ${weatherObj.weather[0].description} ${weatherObj.main.temp - 273.15}  °C outside.`
+                            : `Loading`}
+            </div>
+        )
+    }
+}
+
+export default Weather
