b4a6874226885b73c210733ac94fbfaca2fa2d2b
Christian Broberg <cb@webhouse.dk>
2018-10-15 15:55:53 +0200

    New API key

--- src/GetData.js
+++ src/GetData.js
@@ -8,14 +8,14 @@ const api = create({
 export const GetWeather = async (parameter) => {
     var data;
     if (typeof (parameter) === 'object') {
-        var obj = await api.get('weather?lat=' + parameter.lat + '&lon=' + parameter.lon + '&appid=cbabea7dfe88ded04409be196b518dab')
+        var obj = await api.get('weather?lat=' + parameter.lat + '&lon=' + parameter.lon + '&appid=5e1d43ad492006206d3f953b8b45f42e')
             .then((res) => {
                 if (res.ok) {
                     data = res.data;
                 }
             })
     } else {
-        var obj = await api.get('weather?q=' + parameter + '&appid=cbabea7dfe88ded04409be196b518dab')
+        var obj = await api.get('weather?q=' + parameter + '&appid=5e1d43ad492006206d3f953b8b45f42e')
             .then((res) => {
                 if (res.ok){
                     data = res.data;
